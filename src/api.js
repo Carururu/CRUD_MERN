3e08d258de2e7edd2f12a6afdc183a2f8fdc0dcf
Carururu <cdvsalanga@gmail.com>
2021-08-25 20:35:29 +0800

    Creating Todos

--- src/api.js
+++ src/api.js
@@ -4,3 +4,7 @@ export const getTodos = async () => {
   let result = await axios.get(`http://localhost:4000/`)
   return result.data
 }
+
+export const createTodo = async (todo) => {
+  await axios.post(`http://localhost:4000/create`, todo)
+}
